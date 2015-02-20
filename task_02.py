b7d203bfcd721c9ef7f549074bac6d4f9cf8d5b1
heidyvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-20 02:57:23 +0000

    task_02

--- task_02.py
+++ task_02.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 """A docstring."""
 
-MY_INTEGER = int(raw_input('How old are you?' ))
-print MY_INTEGER, type(MY_INTEGER) 
+MY_INTEGER = int(raw_input('How old are you? '))
+print MY_INTEGER, type(MY_INTEGER)
