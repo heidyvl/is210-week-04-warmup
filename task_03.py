f24d7eb470ef1958fb379a9978e81a49bc63b60e
heidyvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-20 01:44:12 +0000

    task_03

--- task_03.py
+++ task_03.py
@@ -1,8 +1,18 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A complex testcase."""
 
 EXPENSE = 14.23
-LOOKS_NICE = True
-MAX_EXPENSE = 12
+LOOKS_NICE=True
+MAX_EXPENSE=12
 GET_OUT_ALIVE = False
+
+SACRIFICE = (LOOKS_NICE == True) & (EXPENSE <= MAX_EXPENSE) or GET_OUT_ALIVE == False
+
+print SACRIFICE
+    
+    
+
+
+
